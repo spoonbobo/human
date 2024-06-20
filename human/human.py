dce886e8d4c16e1c4679b3ae90edb4b0c674c39c
Season <seasonluke@gmail.com>
2024-06-20 21:24:33 +0800

    update

---
+++ human/human.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+class Human(ABC):
+    """
+    A human is a true intelligence.
+    """
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def observe(self):
+        pass
+    
+    @abstractmethod
+    def think(self):
+        pass
+    
+    @abstractmethod
+    def act(self):
+        pass
+
+    @abstractmethod
+    def learn(self):
+        pass
+    
+    @abstractmethod
+    def feel(self):
+        pass
+    
+    @abstractmethod
+    def experience(self):
+        pass
+    
+    @abstractmethod
+    def create(self):
+        pass
+    
+    @abstractmethod
+    def sense(self):
+        pass
+    
+    @abstractmethod
+    def communicate(self):
+        pass
+    
+    @abstractmethod
+    def decide(self):
+        pass
+
+    @abstractmethod
+    def adapt(self):
+        pass
+    
+    @abstractmethod
+    def empathize(self, input):
+        pass
+    
+    @abstractmethod
+    def understand(self):
+        pass
+
+    @abstractmethod
+    def love(self):
+        pass
